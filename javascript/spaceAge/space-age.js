e8aaceb91c93eb94aac9f47b9ed2889182f722a8
Sidney Rens <sidney.rens.ext@bayer.com>
2021-02-26 13:06:32 -0600

    spaceage

--- javascript/spaceAge/space-age.js
+++ javascript/spaceAge/space-age.js
@@ -1,8 +1,31 @@
-//
-// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const earthYears = (ageInSeconds) => {
+  return ageInSeconds / 31557600
+}
 
-export const age = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+const orbitalPeriod = (planet) => {
+  switch (planet) {
+    case 'mercury':
+      return 0.2408467
+    case 'venus':
+      return 0.61519726
+    case 'earth':
+      return 1.0
+    case 'mars':
+      return 1.8808158
+    case 'jupiter':
+      return 11.862615
+    case 'saturn':
+      return 29.447498
+    case 'uranus':
+      return 84.016846
+    case 'neptune':
+      return 164.79132
+    default: 
+      return "that isn't a planet"
+  }
+}
+
+export const age = (planet, ageInSeconds) => {
+  const earthAge = earthYears(ageInSeconds)
+  return parseFloat((earthAge/orbitalPeriod(planet)).toFixed(2))
+}
