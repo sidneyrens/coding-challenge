032b5384dbee9b4b6b8c097afd114ece0e2b7321
David Yoo <david.yoo@daugherty.com>
2021-02-26 15:33:32 -0600

    solve luhn

--- javascript/luhn/luhn.js
+++ javascript/luhn/luhn.js
@@ -3,6 +3,32 @@
 // convenience to get you started writing code faster.
 //
 
-export const valid = () => {
-  throw new Error("Remove this statement and implement this function");
+export const valid = (number) => {
+  let cleanNumber = number.replace(/ /g, "");
+  if (cleanNumber.length < 2) {
+    return false;
+  }
+  if (isNaN(cleanNumber)) {
+    return false;
+  }
+
+  var numbersToSum = [];
+  let reversedNumbers = cleanNumber.split("").reverse().join("");
+  for (let i = 0; i < reversedNumbers.length; i++) {
+    if (i % 2 != 0) {
+      if (reversedNumbers[i] * 2 > 9) {
+        numbersToSum.push(reversedNumbers[i] * 2 - 9);
+      } else {
+        numbersToSum.push(reversedNumbers[i] * 2);
+      }
+    } else {
+      numbersToSum.push(reversedNumbers[i]);
+    }
+  }
+
+  let sum = numbersToSum.reduce(
+    (a, b) => Number.parseInt(a) + Number.parseInt(b)
+  );
+  
+  return sum % 10 == 0;
 };
