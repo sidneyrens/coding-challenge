9802bf786d531e9104fbf629f62b3c36799a050c
David Yoo <david.yoo@daugherty.com>
2021-02-26 14:12:16 -0600

    solve rnaTranscription

--- javascript/rnaTranscription/rna-transcription.js
+++ javascript/rnaTranscription/rna-transcription.js
@@ -3,6 +3,28 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRna = () => {
-  throw new Error("Remove this statement and implement this function");
+export const toRna = (dna) => {
+  var result = "";
+  Array.from(dna).forEach(nucleotide => {
+    result += mapDnaToRnaNucleotide(nucleotide);
+  });
+  return result
 };
+
+const mapDnaToRnaNucleotide = (nucleotide) => {
+  switch (nucleotide) {
+    case "G":
+      return "C";
+    case "C":
+      return "G";
+    case "T":
+      return "A";
+    case "A":
+      return "U";
+  }
+};
+
+// - `G` -> `C`
+// - `C` -> `G`
+// - `T` -> `A`
+// - `A` -> `U`
